803bcce010a103a8892835269c6b5df8701ee04b
Rhodri-hc <13622450494@163.com>
2023-06-10 20:34:02 +0800

    express 上传

---
+++ express/index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const multer  = require('multer')
+const cors = require('cors');
+
+const app = express()
+app.use(cors());
+
+const upload = multer({ dest: 'uploads/' })
+
+app.post('/aaa', upload.single('aaa'), function (req, res, next) {
+  console.log('req.file', req.file);
+  console.log('req.body', req.body);
+})
+
+app.post('/bbb', upload.array('bbb', 2), function (req, res, next) {
+    console.log('req.files', req.files);
+    console.log('req.body', req.body);
+}, function(err, req, res, next) {
+    if(err instanceof MulterError && err.code === 'LIMIT_UNEXPECTED_FILE') {
+        res.status(400).end('Too many files uploaded');
+    }
+})
+
+app.post('/ccc', upload.fields([
+    { name: 'aaa', maxCount: 3 },
+    { name: 'bbb', maxCount: 2 }
+]), function (req, res, next) {
+    console.log('req.files', req.files);
+    console.log('req.body', req.body);
+})
+
+
+app.post('/ddd', upload.any(), function(req, res, next) {
+    console.log('req.files', req.files);
+    console.log('req.body', req.body);
+});
+
+
+app.listen(3333);
